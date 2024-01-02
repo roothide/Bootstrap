3e8e14e9ba8980c28e0a6316fd3d6922cc456c0b
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-01-02 17:04:40 +0800

    Delete Bootstrap/Bootstrap-Bridging-Header.h

--- Bootstrap/Bootstrap-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#include "bootstrap.h"
-#include "bootstrapFr.h"
-#include "LSApplicationProxy.h"
