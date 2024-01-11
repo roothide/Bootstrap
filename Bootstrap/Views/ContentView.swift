45de231e0ab5ee21e5fcf7463c038473ed31318c
inoshishi0610 <70304204+inoshishi0610@users.noreply.github.com>
2024-01-11 15:14:51 +0700

    Update ContentView.swift

--- Bootstrap/Views/ContentView.swift
+++ Bootstrap/Views/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FluidGradient
+import Foundation
 
 @objc class SwiftUIViewWrapper: NSObject {
     @objc static func createSwiftUIView() -> UIViewController {
@@ -20,173 +21,209 @@ struct ContentView: View {
         return [""]
     }()
     
-    @State private var openSSH = false
-    @State private var showOptions = false
-    @State private var showCredits = false
+    @State var openSSH = false
+    @State var showOptions = false
+    @State var showCredits = false
     let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
     
     var body: some View {
-        ZStack {
-            FluidGradient(blobs: [.red, .orange],
-                          highlights: [.red, .yellow],
-                          speed: 0.5,
-                          blur: 0.95)
-            .background(.quaternary)
+        GeometryReader { geometry in
+            ZStack {
+                FluidGradient(blobs: [.red, .orange],
+                              highlights: [.red, .yellow],
+                              speed: 0.5,
+                              blur: 0.95)
+                .background(.quaternary)
             
-            VStack {
-                HStack(spacing: 15) {
-                    Image("Bootstrap")
-                        .resizable()
-                        .frame(width: 80, height: 80)
-                        .cornerRadius(18)
-                    
-                    VStack(alignment: .leading, content: {
-                        Text("Bootstrap")
-                            .bold()
-                            .font(Font.system(size: 35))
-                        Text("Version \(appVersion!)")
-                            .font(Font.system(size: 20))
-                            .opacity(0.5)
-                    })
-                }
-                .padding(20)
-                
                 VStack {
-                    Button {
-                        bootstrapFr()
-                    } label: {
-                        if isBootstrapInstalled() {
-                            Label(
-                                title: { Text("Kickstart").bold() },
-                                icon: { Image(systemName: "terminal") }
-                            )
-                            .padding(25)
-                        } else {
-                            Label(
-                                title: { Text("Bootstrap").bold() },
-                                icon: { Image(systemName: "terminal") }
-                            )
-                            .padding(25)
-                        }
-                    }
-                    .frame(width: 295)
-                    .background {
-                        Color(UIColor.systemBackground)
-                            .cornerRadius(20)
-                            .opacity(0.5)
-                    }
-                    .disabled(isSystemBootstrapped())
+                    HStack(spacing: 15) {
+                        Image("Bootstrap")
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .cornerRadius(18)
                     
-                    if isBootstrapInstalled() {
+                        VStack(alignment: .leading, content: {
+                            Text(" Bootstrap ")
+                                .bold()
+                                .font(Font.system(size: 35))
+                            Text("AAA")
+                                .font(Font.system(size: 20))
+                                .opacity(0.5)
+                        })
+                    }
+                    .padding(20)
+                
+                    VStack {
                         Button {
-                            unbootstrapFr()
+                            bootstrapFr()
                         } label: {
-                            Label(
-                                title: { Text("Uninstall").bold() },
-                                icon: { Image(systemName: "trash") }
-                            )
-                            .padding(25)
+                            if isBootstrapInstalled() && isSystemBootstrapped() {
+                                Label(
+                                    title: { Text("Kickstart") },
+                                    icon: { Image(systemName: "terminal") }
+                                )
+                                .padding(25)
+                            } else if isBootstrapInstalled() && !isSystemBootstrapped() {
+                                Label(
+                                    title: { Text("Leadstart") },
+                                    icon: { Image(systemName: "terminal") }
+                                )
+                                .padding(25)
+                            } else {
+                                Label(
+                                    title: { Text("Bootstrap") },
+                                    icon: { Image(systemName: "terminal") }
+                                )
+                                .padding(25)
+                            }
                         }
-                        .frame(width: 295)
                         .background {
                             Color(UIColor.systemBackground)
                                 .cornerRadius(20)
                                 .opacity(0.5)
                         }
                         .disabled(isSystemBootstrapped())
-                    }
                     
-                    HStack {
-                        Button {
-                            withAnimation {
-                                showOptions.toggle()
+                        if isBootstrapInstalled() {
+                            Button {
+                                unbootstrapFr()
+                            } label: {
+                                Label(
+                                    title: { Text("Uninstall") },
+                                    icon: { Image(systemName: "trash") }
+                                )
+                                .padding(25)
                             }
-                        } label: {
-                            Label(
-                                title: { Text("Settings") },
-                                icon: { Image(systemName: "gear") }
-                            )
-                            .padding(25)
+                            .background {
+                                Color(UIColor.systemBackground)
+                                    .cornerRadius(20)
+                                    .opacity(0.5)
+                            }
+                            .disabled(isSystemBootstrapped())
+
+                            Button {
+                                rebootFr()
+                            } label: {
+                                Label(
+                                    title: { Text("Reboot") },
+                                    icon: { Image(systemName: "arrow.clockwise.circle.fill") }
+                                )
+                                .padding(25)
+                            }
+                            .background {
+                                Color(UIColor.systemBackground)
+                                    .cornerRadius(20)
+                                    .opacity(0.5)
+                            }
+                            .disabled(!isSystemBootstrapped())
                         }
-                        .background {
-                            Color(UIColor.systemBackground)
-                                .cornerRadius(20)
-                                .opacity(0.5)
-                        }
+                    
+                        HStack {
+                            Button {
+                                withAnimation {
+                                    showOptions.toggle()
+                                }
+                            } label: {
+                                Label(
+                                    title: { Text("Settings") },
+                                    icon: { Image(systemName: "gear") }
+                                )
+                                .padding(25)
+                            }
+                            .background {
+                                Color(UIColor.systemBackground)
+                                    .cornerRadius(20)
+                                    .opacity(0.5)
+                            }
                         
-                        Button {
-                            respringFr()
-                        } label: {
-                            Label(
-                                title: { Text("Respring") },
-                                icon: { Image(systemName: "arrow.clockwise") }
-                            )
-                            .padding(25)
+                            Button {
+                                respringFr()
+                            } label: {
+                                Label(
+                                    title: { Text("Respring") },
+                                    icon: { Image(systemName: "arrow.clockwise") }
+                                )
+                                .padding(25)
+                            }
+                            .background {
+                                Color(UIColor.systemBackground)
+                                    .cornerRadius(20)
+                                    .opacity(0.5)
+                            }
+                            .disabled(!isSystemBootstrapped())
                         }
-                        .background {
-                            Color(UIColor.systemBackground)
-                                .cornerRadius(20)
-                                .opacity(0.5)
-                        }
-                        .disabled(!isSystemBootstrapped())
-                    }
                     
-                    VStack {
-                        ScrollView {
-                            ScrollViewReader { scroll in
-                                VStack(alignment: .leading) {
-                                    ForEach(0..<LogItems.count, id: \.self) { LogItem in
-                                        Text("\(String(LogItems[LogItem]))")
-                                            .textSelection(.enabled)
-                                            .font(.custom("Menlo", size: 15))
-                                            .foregroundColor(.white)
+                        VStack {
+                            ScrollView {
+                                ScrollViewReader { scroll in
+                                    VStack(alignment: .leading) {
+                                        ForEach(0..<LogItems.count, id: \.self) { LogItem in
+                                            Text("\(String(LogItems[LogItem]))")
+                                                .textSelection(.enabled)
+                                                .font(.custom("Menlo", size: 15))
+                                                .foregroundColor(.white)
+                                        }
                                     }
-                                }
-                                .onReceive(NotificationCenter.default.publisher(for: LogStream.shared.reloadNotification)) { obj in
-                                    DispatchQueue.global(qos: .utility).async {
-                                        FetchLog()
-                                        scroll.scrollTo(LogItems.count - 1)
+                                    .onReceive(NotificationCenter.default.publisher(for: LogStream.shared.reloadNotification)) { obj in
+                                        DispatchQueue.global(qos: .utility).async {
+                                            FetchLog()
+                                            scroll.scrollTo(LogItems.count - 1)
+                                        }
                                     }
                                 }
                             }
+                            .frame(height: 150)
                         }
-                        .frame(height: 150)
-                    }
-                    .frame(width: 253)
-                    .padding(20)
-                    .background {
-                        Color(.black)
-                            .cornerRadius(20)
-                            .opacity(0.5)
-                    }
+                        .frame(width: 253)
+                        .padding(20)
+                        .background {
+                            Color(.black)
+                                .cornerRadius(20)
+                                .opacity(0.5)
+                        }
                     
-                    Text("UI made with love by haxi0. ♡")
-                        .font(Font.system(size: 13))
-                        .opacity(0.5)
+                        Text("♡ Update for inoshishi0610 ♡")
+                            .font(Font.system(size: 13))
+                            .opacity(0.5)
+
+                        Button {
+                            let link = "https://github.com/inoshishi0610/NoZebra/releases/tag/release"
+                            if let url = URL(string: link) {
+                                UIApplication.shared.open(url)
+                            }
+                        } label: {
+                            Label(
+                                title: { Text("Button_Update_Available") },
+                                icon: { Image(systemName: "arrow.down.circle") }
+                            )
+                            .font(Font.system(size: 15))
+                            .opacity(0.5)
+                        }    
+                    }
                 }
-            }
-            
-            Button {
-                withAnimation {
-                    showCredits.toggle()
+
+                Button {
+                    withAnimation {
+                        showCredits.toggle()
+                    }
+                } label: {
+                    Label(
+                        title: { Text("Credits") },
+                        icon: { Image(systemName: "person") }
+                    )
                 }
-            } label: {
-                Label(
-                    title: { Text("Credits") },
-                    icon: { Image(systemName: "person") }
-                )
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                .padding(25)
             }
-            .frame(maxHeight: .infinity, alignment: .bottom)
-            .padding(25)
-        }
-        .ignoresSafeArea()
-        .overlay {
-            if showCredits {
-                CreditsView(showCredits: $showCredits)
-            }
+            .ignoresSafeArea()
+            .overlay {
+                if showCredits {
+                    CreditsView(showCredits: $showCredits)
+                }
             
-            if showOptions {
-                OptionsView(showOptions: $showOptions, openSSH: $openSSH)
+                if showOptions {
+                    OptionsView(showOptions: $showOptions, openSSH: $openSSH)
+                }
             }
         }
         .onAppear {
