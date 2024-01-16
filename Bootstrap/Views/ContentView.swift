8eaa8ce146a9758d59c2bb32e8c895706aef7412
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-01-17 07:18:31 +0800

    Update ContentView.swift

--- Bootstrap/Views/ContentView.swift
+++ Bootstrap/Views/ContentView.swift
@@ -239,7 +239,7 @@ struct ContentView: View {
     
     func checkForUpdates() async throws {
         if let currentAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
-            let owner = "roothide"
+            let owner = "wwg135"
             let repo = "Bootstrap"
             
             // Get the releases
