5d9f772edd2c802d9111f30ac3fdd5cea1794c57
roothider <158083651+roothider@users.noreply.github.com>
2025-12-06 02:37:51 +0800

    ignore built basebin

--- Bootstrap/basebin/test.sh
+++
@@ -1,3 +0,0 @@
-set jbroot_path $(realpath ..)
-export DYLD_INSERT_LIBRARIES=$jbroot_path/basebin/bootstrap.dylib
-DYLD_INSERT_LIBRARIES=$jbroot_path/basebin/bootstrap.dylib  $jbroot_path/usr/bin/zsh
